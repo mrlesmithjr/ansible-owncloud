a810fb01a2148729dd6c8306fcced4ff0cb4eb65
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-01-09 00:33:37 -0500

    Changed install method to manual install
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- provision.sh
+++ provision.sh
@@ -3,7 +3,5 @@ sudo apt-get update
 sudo apt-get install -y git python-pip python-dev
 sudo pip install jinja2
 sudo pip install ansible
-sudo git clone https://github.com/mrlesmithjr/ansible-owncloud.git /etc/ansible/roles/ansible-owncloud
-sudo git clone https://github.com/mrlesmithjr/ansible-mysql.git /etc/ansible/roles/ansible-mysql
-sudo git clone https://github.com/mrlesmithjr/ansible-apt-mirror.git /etc/ansible/roles/ansible-apt-mirror
+sudo ansible-galaxy install -r /vagrant/requirements.yml -f
 ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
